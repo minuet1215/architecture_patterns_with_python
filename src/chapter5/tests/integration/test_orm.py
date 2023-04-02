1fb51838862eb95b3e74322c5b957ad5d908aee7
minu <brotheratoz3@naver.com>
2023-04-02 14:49:49 +0900

    fix: explicit declaration query text

--- src/chapter5/tests/integration/test_orm.py
+++ src/chapter5/tests/integration/test_orm.py
@@ -1,13 +1,14 @@
 from domain import model
 from datetime import date
+from sqlalchemy.sql import text
 
 
 def test_orderline_mapper_can_load_lines(session):
-    session.execute(
+    session.execute(text(
         "INSERT INTO order_lines (orderid, sku, qty) VALUES "
         '("order1", "RED-CHAIR", 12),'
         '("order1", "RED-TABLE", 13),'
-        '("order2", "BLUE-LIPSTICK", 14)'
+        '("order2", "BLUE-LIPSTICK", 14)')
     )
     expected = [
         model.OrderLine("order1", "RED-CHAIR", 12),
@@ -22,18 +23,19 @@ def test_orderline_mapper_can_save_lines(session):
     session.add(new_line)
     session.commit()
 
-    rows = list(session.execute('SELECT orderid, sku, qty FROM "order_lines"'))
+    rows = list(session.execute(
+        text('SELECT orderid, sku, qty FROM "order_lines"')))
     assert rows == [("order1", "DECORATIVE-WIDGET", 12)]
 
 
 def test_retrieving_batches(session):
-    session.execute(
+    session.execute(text(
         "INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
-        ' VALUES ("batch1", "sku1", 100, null)'
+        ' VALUES ("batch1", "sku1", 100, null)')
     )
-    session.execute(
+    session.execute(text(
         "INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
-        ' VALUES ("batch2", "sku2", 200, "2011-04-11")'
+        ' VALUES ("batch2", "sku2", 200, "2011-04-11")')
     )
     expected = [
         model.Batch("batch1", "sku1", 100, eta=None),
@@ -47,8 +49,8 @@ def test_saving_batches(session):
     batch = model.Batch("batch1", "sku1", 100, eta=None)
     session.add(batch)
     session.commit()
-    rows = session.execute(
-        'SELECT reference, sku, _purchased_quantity, eta FROM "batches"'
+    rows = session.execute(text(
+        'SELECT reference, sku, _purchased_quantity, eta FROM "batches"')
     )
     assert list(rows) == [("batch1", "sku1", 100, None)]
 
@@ -59,28 +61,29 @@ def test_saving_allocations(session):
     batch.allocate(line)
     session.add(batch)
     session.commit()
-    rows = list(session.execute('SELECT orderline_id, batch_id FROM "allocations"'))
+    rows = list(session.execute(text(
+        'SELECT orderline_id, batch_id FROM "allocations"')))
     assert rows == [(batch.id, line.id)]
 
 
 def test_retrieving_allocations(session):
-    session.execute(
-        'INSERT INTO order_lines (orderid, sku, qty) VALUES ("order1", "sku1", 12)'
+    session.execute(text(
+        'INSERT INTO order_lines (orderid, sku, qty) VALUES ("order1", "sku1", 12)')
     )
-    [[olid]] = session.execute(
-        "SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku",
+    [[olid]] = session.execute(text(
+        "SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku"),
         dict(orderid="order1", sku="sku1"),
     )
-    session.execute(
+    session.execute(text(
         "INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
-        ' VALUES ("batch1", "sku1", 100, null)'
+        ' VALUES ("batch1", "sku1", 100, null)')
     )
-    [[bid]] = session.execute(
-        "SELECT id FROM batches WHERE reference=:ref AND sku=:sku",
+    [[bid]] = session.execute(text(
+        "SELECT id FROM batches WHERE reference=:ref AND sku=:sku"),
         dict(ref="batch1", sku="sku1"),
     )
-    session.execute(
-        "INSERT INTO allocations (orderline_id, batch_id) VALUES (:olid, :bid)",
+    session.execute(text(
+        "INSERT INTO allocations (orderline_id, batch_id) VALUES (:olid, :bid)"),
         dict(olid=olid, bid=bid),
     )
 
